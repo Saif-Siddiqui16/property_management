d9336c8f3e2baee39c5d47dd573847618c3d5270
Saif-Siddiqui16 <Saif-Siddiqui16@users.noreply.github.com>
2026-02-09 10:52:10 -0800

    recent

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+    baseURL: 'http://localhost:5000',
+    //baseURL: 'https://property-saif-production.up.railway.app/',
+    //baseURL: 'https://saif-property-6-fab-backend-production.up.railway.app/',
+    //baseURL: 'https://api.kiaantechnology.com/',
     headers: {
         'Content-Type': 'application/json',
     },
